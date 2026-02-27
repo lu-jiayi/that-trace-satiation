fad0029732096234a9e14c14ec08e4ac06b523ec
Jiayi Lu <60253964+lu-jiayi@users.noreply.github.com>
2026-02-26 23:06:24 -0600

    upload exp

---
+++ experiment/exp1/js/index.js
@@ -0,0 +1,413 @@
+///Don't touch lines 2-43.
+var order = 1;
+/// Helper function that shuffles an array. Don't touch.
+var shuffle = function (array) {
+
+	var currentIndex = array.length;
+	var temporaryValue, randomIndex;
+	while (0 !== currentIndex) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
+
+};
+///Helper functions to get random selection from array, and to remove elements from array. Don't touch.
+function getRandomNonDuplicateSelection(arr, count, exclusionArr) {
+  var selected = [];
+  var available = arr.filter(item => !exclusionArr.includes(item));
+
+  if (available.length < count) {
+    throw new Error('Insufficient unique elements available for selection.');
+  }
+
+  for (var i = 0; i < count; i++) {
+    var randomIndex = Math.floor(Math.random() * available.length);
+    selected.push(available[randomIndex]);
+    available.splice(randomIndex, 1);
+  }
+  return selected;
+}
+
+function removeFromArray(arr, elements) {
+  for (var i = 0; i < elements.length; i++) {
+    var index = arr.indexOf(elements[i]);
+    if (index > -1) {
+      arr.splice(index, 1);
+    }
+  }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function generateStimulusList() {
+  var numBlocks = 6;
+
+  // Items 1..12
+  var allItems = [];
+  for (var i = 1; i <= 12; i++) allItems.push(i);
+
+  // Fillers
+  var grammaticalFillers = [];
+  var ungrammaticalFillers = [];
+  for (var j = 1; j <= 12; j++) {
+    grammaticalFillers.push(8000 + j);    // 8001–8012
+    ungrammaticalFillers.push(9000 + j);  // 9001–9012
+  }
+
+  // Randomly split items into 6 for cond1 and 6 for cond2
+  shuffle(allItems);
+  var cond1Items = allItems.slice(0, numBlocks);        // 6 items
+  var cond2Items = allItems.slice(numBlocks, 2*numBlocks); // other 6 items
+
+  // (Optional) reshuffle within each condition group so pairing is random
+  shuffle(cond1Items);
+  shuffle(cond2Items);
+
+  var blocks = [];
+
+  for (var b = 0; b < numBlocks; b++) {
+    var itemCond1 = cond1Items[b];
+    var itemCond2 = cond2Items[b];
+
+    // Build critical IDs numerically: 1AB1 or 1AB2
+    var crit1 = 1000 + (itemCond1 * 10) + 1; // condition 1
+    var crit2 = 1000 + (itemCond2 * 10) + 2; // condition 2 (different item)
+
+    // Pick one grammatical filler (no repeats)
+    var gram = getRandomNonDuplicateSelection(grammaticalFillers, 1, [])[0];
+    removeFromArray(grammaticalFillers, [gram]);
+
+    // Pick one ungrammatical filler (no repeats)
+    var ungram = getRandomNonDuplicateSelection(ungrammaticalFillers, 1, [])[0];
+    removeFromArray(ungrammaticalFillers, [ungram]);
+
+    var block = [crit1, crit2, gram, ungram];
+
+    // Shuffle within block
+    shuffle(block);
+
+    blocks.push(block);
+  }
+
+  // Shuffle block order but keep blocks intact
+  shuffle(blocks);
+console.log(blocks);
+  // Flatten
+  var presentation_list = [];
+  for (var k = 0; k < blocks.length; k++) {
+    presentation_list = presentation_list.concat(blocks[k]);
+  }
+
+  return presentation_list;
+}
+
+// Store full list here (integers)
+var presentation_list = generateStimulusList();
+console.log(presentation_list);
+
+////Now, we have a list of sentence IDs. We need to look up the actual sentences corresponding to the IDs in the stimuli.js file. 
+const populated_list = presentation_list.map((integer) => all_stimuli.find((item) => item.unique_id === integer));
+console.log(populated_list);
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////This section contain all the types of slides that will be used by the html file (e.g. practice slides, feedback slides, critial trial slides, demographic info slides, thank-you slide, etc.).////////
+////////You can just modify the templates based on the need of your experiment. The most important thing is to update the "log_responses" functions, so that you log all the information you need.    ////////  
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function make_slides(f) {
+  var slides = {};  
+  slides.i0 = slide({
+     name : "i0",
+     start: function() {
+      exp.startT = Date.now();
+     }
+  });
+
+  slides.instructions = slide({
+    name : "instructions",
+    button : function() {
+      exp.go(); //use exp.go() if and only if there is no "present" data.
+    }
+  });
+
+  slides.single_trial = slide({
+    name: "single_trial",
+    start: function() {
+      $(".err").hide();
+      $(".display_condition").html("You are in " + exp.condition + ".");
+    },
+    button : function() {
+      response = $("#text_response").val();
+      if (response.length == 0) {
+        $(".err").show();
+      } else {
+        exp.data_trials.push({
+          "trial_type" : "single_trial",
+          "response" : response
+        });
+        exp.go(); //make sure this is at the *end*, after you log your data
+      }
+    },
+  });
+
+  slides.practice_slider = slide({
+    name : "practice_slider",
+
+    /* trial information for this block
+     (the variable 'stim' will change between each of these values,
+      and for each of these, present_handle will be run.) */
+    present : [{"a": 1}],
+    //this gets run only at the beginning of the block
+    present_handle : function(stim) {
+      $(".err").hide();
+      $(".errgood").hide();
+      this.stim = stim;
+      $(".prompt").html("Who did you send the letter to?");
+      this.init_sliders();
+      exp.sliderPost = null; //erase current slider value
+      exp.first_response_wrong = 0;
+      exp.first_response_value = null;
+      exp.attempts = 0;
+    },
+    button : function() {
+      if (exp.sliderPost == null) {
+        $(".err").show();
+      } 
+      else if (exp.sliderPost < 0.5) {
+        exp.first_response_wrong = 1;
+        exp.first_response_value =exp.sliderPost;
+        exp.attempts = exp.attempts + 1;
+        $(".errgood").show();
+      }
+      else {
+        this.log_responses();
+        /* use _stream.apply(this); if and only if there is
+        "present" data. (and only *after* responses are logged) */
+        _stream.apply(this);
+      }
+    },
+    init_sliders : function() {
+      utils.make_slider("#practice_slider_1", function(event, ui) {
+        exp.sliderPost = ui.value;
+      });
+    },
+    log_responses : function() {
+      exp.data_trials.push({
+        "response" : exp.sliderPost,
+        "first_response_value": exp.first_response_value,
+        "wrong_attempts": exp.attempts,
+        "item_type" : "practice_good",
+        "item_number": "practice_good",
+        "trial_sequence_total": 0
+      });
+
+    }
+  });
+
+
+  slides.post_practice_1 = slide({
+    name : "post_practice_1",
+    button : function() {
+      exp.go(); //use exp.go() if and only if there is no "present" data.
+    }
+  });
+
+
+ 
+
+  slides.practice_slider_bad = slide({
+    name : "practice_slider_bad",
+
+    /* trial information for this block
+     (the variable 'stim' will change between each of these values,
+      and for each of these, present_handle will be run.) */
+    present : [1],
+
+  
+    //this gets run only at the beginning of the block
+    present_handle : function(stim) {
+      $(".err").hide();
+      $(".errbad").hide();
+      $(".prompt").html("Who email did send he to?");
+      this.init_sliders();
+      exp.sliderPost = null; //erase current slider value
+      exp.first_response_wrong = 0;
+      exp.first_response_value = null;
+      exp.attempts = 0;
+    },
+    button : function() {
+      if (exp.sliderPost == null) {
+        $(".err").show();
+      } 
+      else if (exp.sliderPost > 0.5) {
+        exp.first_response_wrong = 1;
+        exp.first_response_value = exp.sliderPost;
+        exp.attempts = exp.attempts + 1;
+        $(".errbad").show();
+      }
+      else {
+        this.log_responses();
+        /* use _stream.apply(this); if and only if there is
+        "present" data. (and only *after* responses are logged) */
+        _stream.apply(this);
+      }
+    },
+    init_sliders : function() {
+      utils.make_slider("#practice_slider_2", function(event, ui) {
+        exp.sliderPost = ui.value;
+        
+      });
+    },
+    log_responses : function() {
+      exp.data_trials.push({
+        "response" : exp.sliderPost,
+        "first_response_value": exp.first_response_value,
+        "wrong_attempts": exp.attempts,
+        "item_type" : "practice_bad",
+        "item_number": "practice_bad",
+        "trial_sequence_total": 0
+      });
+
+    }
+  });
+
+  slides.post_practice_2 = slide({
+    name : "post_practice_2",
+    button : function() {
+      exp.go(); //use exp.go() if and only if there is no "present" data.
+    }
+  });
+
+
+  slides.last_reminder = slide({
+    name : "last_reminder",
+    button : function() {
+      exp.go(); //use exp.go() if and only if there is no "present" data.
+    }
+    
+  });
+
+ 
+
+  slides.one_slider = slide({
+    name : "one_slider",
+
+    /* trial information for this block
+     (the variable 'stim' will change between each of these values,
+      and for each of these, present_handle will be run.) */
+    present : populated_list,
+    
+    //this gets run only at the beginning of the block
+    present_handle : function(stim) {
+      $(".err").hide();
+      this.stim = stim; //I like to store this information in the slide so I can record it later.
+      $(".target").html(stim.sentence);
+      this.init_sliders()
+      exp.sliderPost = null; //erase current slider value
+    },
+
+    button : function() {
+      if (exp.sliderPost == null) {
+        $(".err").show();
+      } else {
+        this.log_responses();
+
+        /* use _stream.apply(this); if and only if there is
+        "present" data. (and only *after* responses are logged) */
+        _stream.apply(this);
+      }
+    },
+
+    init_sliders : function() {
+      utils.make_slider("#single_slider", function(event, ui) {
+        exp.sliderPost = ui.value;
+      });
+    },
+
+    log_responses : function() {
+      exp.data_trials.push({
+        // item-specific fields
+        "response" : exp.sliderPost,
+        "lexicalization": this.stim.lexicalization,
+        "sentence": this.stim.sentence,
+        "gap_position": this.stim.gap_position,
+        "complementizer": this.stim.complementizer,
+        "trial_sequence_total": order,
+        "sentence_id": this.stim.unique_id
+      });
+      order = order + 1;
+    }
+  });
+
+  slides.subj_info =  slide({
+    name : "subj_info",
+    submit : function(e){
+      //if (e.preventDefault) e.preventDefault(); // I don't know what this means.
+      exp.subj_data = {
+        asses : $('input[name="assess"]:checked').val(),
+        age : $("#age").val(),
+        gender : $("#gender").val(),
+        education : $("#education").val(),
+        comments : $("#comments").val(),
+        fairprice: $("#fairprice").val()
+      };
+      exp.go(); //use exp.go() if and only if there is no "present" data.
+    }
+  });
+
+  slides.thanks = slide({
+    name : "thanks",
+    start : function() {
+      exp.data= {
+          "trials" : exp.data_trials,
+          "catch_trials" : exp.catch_trials,
+          "system" : exp.system,
+          "subject_information" : exp.subj_data,
+          "time_in_minutes" : (Date.now() - exp.startT)/60000
+      };
+      proliferate.submit(exp.data);
+    }
+  });
+
+  return slides;
+}
+
+/// init ///
+function init() {
+  exp.trials = [];
+  exp.catch_trials = [];
+  //exp.condition = _.sample(["condition 1", "condition 2"]); //can randomize between subject conditions here
+  exp.system = {
+      Browser : BrowserDetect.browser,
+      OS : BrowserDetect.OS,
+      screenH: screen.height,
+      screenUH: exp.height,
+      screenW: screen.width,
+      screenUW: exp.width
+    };
+  //blocks of the experiment:
+  exp.structure=["i0", "instructions", "practice_slider", "post_practice_1", "practice_slider_bad", "post_practice_2", "last_reminder", 'one_slider', 'subj_info', 'thanks'];
+
+  exp.data_trials = [];
+  //make corresponding slides:
+  exp.slides = make_slides(exp);
+
+  exp.nQs = utils.get_exp_length(); //this does not work if there are stacks of stims (but does work for an experiment with this structure)
+                    //relies on structure and slides being defined
+
+  $('.slide').hide(); //hide everything
+
+  //make sure turkers have accepted HIT (or you're not in mturk)
+  $("#start_button").click(function() {
+    exp.go();
+  });
+
+  exp.go(); //show first slide
+}
---
+++ experiment/exp1/js/stimuli.js
@@ -0,0 +1,293 @@
+const all_stimuli = 
+
+[
+    {
+        "item": 1,
+        "condition": "that",
+        "sentence": "Julia knows the students who you believe that submitted the assignment on time.",
+        "unique_id": 1011
+    },
+    {
+        "item": 1,
+        "condition": null,
+        "sentence": "Julia knows the students who you believe submitted the assignment on time.",
+        "unique_id": 1012
+    },
+    {
+        "item": 2,
+        "condition": "that",
+        "sentence": "Mary recognizes the employees who we think that completed the report very early.",
+        "unique_id": 1021
+    },
+    {
+        "item": 2,
+        "condition": null,
+        "sentence": "Mary recognizes the employees who we think completed the report very early.",
+        "unique_id": 1022
+    },
+    {
+        "item": 3,
+        "condition": "that",
+        "sentence": "Sophia identifies the researchers who we assume that published the article last year.",
+        "unique_id": 1031
+    },
+    {
+        "item": 3,
+        "condition": null,
+        "sentence": "Sophia identifies the researchers who we assume published the article last year.",
+        "unique_id": 1032
+    },
+    {
+        "item": 4,
+        "condition": "that",
+        "sentence": "Kate calls the workers who you say that filed the complaint this week.",
+        "unique_id": 1041
+    },
+    {
+        "item": 4,
+        "condition": null,
+        "sentence": "Kate calls the workers who you say filed the complaint this week.",
+        "unique_id": 1042
+    },
+    {
+        "item": 5,
+        "condition": "that",
+        "sentence": "Janice likes the teachers who we claim that reviewed the manuscript quite early.",
+        "unique_id": 1051
+    },
+    {
+        "item": 5,
+        "condition": null,
+        "sentence": "Janice likes the teachers who we claim reviewed the manuscript quite early.",
+        "unique_id": 1052
+    },
+    {
+        "item": 6,
+        "condition": "that",
+        "sentence": "Emily meets the colleagues who you suppose that finalized the plan right away.",
+        "unique_id": 1061
+    },
+    {
+        "item": 6,
+        "condition": null,
+        "sentence": "Emily meets the colleagues who you suppose finalized the plan right away.",
+        "unique_id": 1062
+    },
+    {
+        "item": 7,
+        "condition": "that",
+        "sentence": "Jake encounters the secretaries who we believe that scheduled the meeting this morning.",
+        "unique_id": 1071
+    },
+    {
+        "item": 7,
+        "condition": null,
+        "sentence": "Jake encounters the secretaries who we believe scheduled the meeting this morning.",
+        "unique_id": 1072
+    },
+    {
+        "item": 8,
+        "condition": "that",
+        "sentence": "Charles notices the experts who you think that updated the software last month.",
+        "unique_id": 1081
+    },
+    {
+        "item": 8,
+        "condition": null,
+        "sentence": "Charles notices the experts who you think updated the software last month.",
+        "unique_id": 1082
+    },
+    {
+        "item": 9,
+        "condition": "that",
+        "sentence": "Ben respects the professors who we assume that established the system long ago.",
+        "unique_id": 1091
+    },
+    {
+        "item": 9,
+        "condition": null,
+        "sentence": "Ben respects the professors who we assume established the system long ago.",
+        "unique_id": 1092
+    },
+    {
+        "item": 10,
+        "condition": "that",
+        "sentence": "Daniel remembers the agents who you say that archived the document pretty efficiently.",
+        "unique_id": 1101
+    },
+    {
+        "item": 10,
+        "condition": null,
+        "sentence": "Daniel remembers the agents who you say archived the document pretty efficiently.",
+        "unique_id": 1102
+    },
+    {
+        "item": 11,
+        "condition": "that",
+        "sentence": "Paul visits the technicians who we claim that processed the application earlier today.",
+        "unique_id": 1111
+    },
+    {
+        "item": 11,
+        "condition": null,
+        "sentence": "Paul visits the technicians who we claim processed the application earlier today.",
+        "unique_id": 1112
+    },
+    {
+        "item": 12,
+        "condition": "that",
+        "sentence": "Derek supports the directors who you suppose that approved the proposal that evening.",
+        "unique_id": 1121
+    },
+    {
+        "item": 12,
+        "condition": null,
+        "sentence": "Derek supports the directors who you suppose approved the proposal that evening.",
+        "unique_id": 1122
+    },
+    {
+        "item": 8001,
+        "condition": "filler_good",
+        "sentence": "Joshua hates eating sushi. ",
+        "unique_id": 8001
+    },
+    {
+        "item": 8002,
+        "condition": "filler_good",
+        "sentence": "Barry likes drinking milkshakes. ",
+        "unique_id": 8002
+    },
+    {
+        "item": 8003,
+        "condition": "filler_good",
+        "sentence": "Lisa likes taking vacations.",
+        "unique_id": 8003
+    },
+    {
+        "item": 8004,
+        "condition": "filler_good",
+        "sentence": "Ali hates missing work. ",
+        "unique_id": 8004
+    },
+    {
+        "item": 8005,
+        "condition": "filler_good",
+        "sentence": "Vivian attempted to write short stories.",
+        "unique_id": 8005
+    },
+    {
+        "item": 8006,
+        "condition": "filler_good",
+        "sentence": "Tommy attempted to take a picture.",
+        "unique_id": 8006
+    },
+    {
+        "item": 8007,
+        "condition": "filler_good",
+        "sentence": "Valerie attempted to cancel her magazine subscription.",
+        "unique_id": 8007
+    },
+    {
+        "item": 8008,
+        "condition": "filler_good",
+        "sentence": "Joey attempted to escape through a window. ",
+        "unique_id": 8008
+    },
+    {
+        "item": 8009,
+        "condition": "filler_good",
+        "sentence": "It seems to Carla that Robert studied for the test effectively.",
+        "unique_id": 8009
+    },
+    {
+        "item": 8010,
+        "condition": "filler_good",
+        "sentence": "It seems to Dave that Gregory left deliberately. ",
+        "unique_id": 8010
+    },
+    {
+        "item": 8011,
+        "condition": "filler_good",
+        "sentence": "It seems to Nathan that Keith finished the work efficiently.",
+        "unique_id": 8011
+    },
+    {
+        "item": 8012,
+        "condition": "filler_good",
+        "sentence": "It seems to Mike that Martha helped the student generously. ",
+        "unique_id": 8012
+    },
+    {
+        "item": 9001,
+        "condition": "filler_bad",
+        "sentence": "John was kissed John.",
+        "unique_id": 9001
+    },
+    {
+        "item": 9002,
+        "condition": "filler_bad",
+        "sentence": "The money was stolen the money.",
+        "unique_id": 9002
+    },
+    {
+        "item": 9003,
+        "condition": "filler_bad",
+        "sentence": "The hedges were trimmed the hedges.",
+        "unique_id": 9003
+    },
+    {
+        "item": 9004,
+        "condition": "filler_bad",
+        "sentence": "The student was punished the student.",
+        "unique_id": 9004
+    },
+    {
+        "item": 9005,
+        "condition": "filler_bad",
+        "sentence": "Who was that the principal would fire expected by the reporters?",
+        "unique_id": 9005
+    },
+    {
+        "item": 9006,
+        "condition": "filler_bad",
+        "sentence": "What was that the movers would break feared by the family?",
+        "unique_id": 9006
+    },
+    {
+        "item": 9007,
+        "condition": "filler_bad",
+        "sentence": "Who was that Martian kidnapped believed by no one?",
+        "unique_id": 9007
+    },
+    {
+        "item": 9008,
+        "condition": "filler_bad",
+        "sentence": "Who is that the jail must release accepted by few citizens?",
+        "unique_id": 9008
+    },
+    {
+        "item": 9009,
+        "condition": "filler_bad",
+        "sentence": "Promise to wash, Neal did the car.",
+        "unique_id": 9009
+    },
+    {
+        "item": 9010,
+        "condition": "filler_bad",
+        "sentence": "Want to eat, James did the pie.",
+        "unique_id": 9010
+    },
+    {
+        "item": 9011,
+        "condition": "filler_bad",
+        "sentence": "Refuse to write, Maria did the article.",
+        "unique_id": 9011
+    },
+    {
+        "item": 9012,
+        "condition": "filler_bad",
+        "sentence": "Tried to bury, Kimberly did the treasure chest.",
+        "unique_id": 9012
+    }
+]
+
